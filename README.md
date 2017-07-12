07dadee8c71093fe290d77caeb01059b366eb834
mail <you@example.com>
2017-07-12 22:04:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Upscale Token (UPS)
+
+PoS Only
+Supply: 100 Millions 
+12 hours minimum stake aged
+
+RPCPORT: 4445
+PORT: 4444
---
+++ src/genesis.h
@@ -0,0 +1,13 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("0x000002ef49b404318beab5397b7b626c6b0670a56065e21f3540b8fb997dbfdd");
+static const uint256 hashGenesisBlockTestNet("");
+
+
+
+#endif // GENESIS_H
